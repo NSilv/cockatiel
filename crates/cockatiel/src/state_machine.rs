e725e309a9a2cb72621560ff34b72ae0503564b5
Nicholas Silvestrin <n.silvestrin98@gmail.com>
2025-04-30 14:20:21 +0200

    code extracted from game, minimal changes

---
+++ crates/cockatiel/src/state_machine.rs
@@ -0,0 +1,270 @@
+/*
+
+animation! {
+  states = [idle, move, hurt]
+  animations = [idle_animation, move_animation, hurt_animation]
+  inputs = [is_moving : bool, is_hurt : bool]
+  transitions {
+    enter -> idle
+    idle -> move, when: is_moving, wait: true
+    any -> hurt, when: is_hurt
+  }
+}
+*/
+
+use std::hash::Hash;
+
+use bevy::{log::debug, utils::HashMap};
+
+#[derive(Clone, Debug, PartialEq)]
+#[allow(dead_code)]
+pub enum InputValue {
+  Boolean(bool),
+  Float(f32),
+}
+pub trait AnimationInput: Clone + Default + std::fmt::Debug + Send + Sync {
+  type Vars: Clone + std::fmt::Debug + Send + Sync;
+  fn get(&self, var: &Self::Vars) -> InputValue;
+}
+
+#[derive(Clone, Debug)]
+#[allow(dead_code)]
+pub enum Condition<Input: AnimationInput> {
+  IsTrue(<Input as AnimationInput>::Vars),
+  IsFalse(<Input as AnimationInput>::Vars),
+  And(Box<Condition<Input>>, Box<Condition<Input>>),
+}
+impl<Input: AnimationInput> Condition<Input> {
+  fn evaluate(&self, input: &Input) -> bool {
+    match self {
+      Condition::IsTrue(var) => match input.get(var) {
+        InputValue::Boolean(b) => b,
+        _ => panic!("input value is not boolean"),
+      },
+      Condition::IsFalse(var) => match input.get(var) {
+        InputValue::Boolean(b) => !b,
+        _ => panic!("input value is not boolean"),
+      },
+      Condition::And(a, b) => a.evaluate(input) && b.evaluate(input),
+    }
+  }
+}
+
+pub trait AnimationState: Eq + Hash + Clone + std::fmt::Debug + Send + Sync {
+  fn enter_state() -> Self;
+  fn exit_state() -> Self;
+  fn any_state() -> Self;
+}
+
+pub trait AnimationEvent: bevy::prelude::Event + Clone + std::fmt::Debug + Send + Sync {}
+#[derive(Clone, Debug)]
+pub struct Transition<State: AnimationState, Inputs: AnimationInput, Shift: AnimationShift> {
+  from: State,
+  to: State,
+  wait_until_finished: bool,
+  condition: Option<Condition<Inputs>>,
+  shift: Option<Shift>,
+}
+impl<State: AnimationState, Inputs: AnimationInput, Shift: AnimationShift>
+  Transition<State, Inputs, Shift>
+{
+  pub fn new(
+    from: State,
+    to: State,
+    wait_until_finished: bool,
+    condition: Option<Condition<Inputs>>,
+    shift: Option<Shift>,
+  ) -> Self {
+    Self {
+      from,
+      to,
+      wait_until_finished,
+      condition,
+      shift,
+    }
+  }
+}
+pub trait AnimationShift: std::fmt::Debug + Send + Sync + Ord + Clone + Hash + Copy {}
+
+#[derive(Debug)]
+pub struct Machine<Input: AnimationInput, State: AnimationState, Shift: AnimationShift> {
+  current_state: State,
+  transitions: HashMap<State, Vec<Transition<State, Input, Shift>>>,
+  transitions_from_any: Vec<Transition<State, Input, Shift>>, // transitions: HashMap<u8, u8>,
+  transitions_from_shift: HashMap<Shift, Transition<State, Input, Shift>>,
+}
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub struct AnimationStepResult<State: AnimationState> {
+  pub state: State,
+  pub changed: bool,
+}
+
+impl<Inputs: AnimationInput, State: AnimationState, Shift: AnimationShift>
+  Machine<Inputs, State, Shift>
+{
+  pub fn new(transitions: Vec<Transition<State, Inputs, Shift>>) -> Self {
+    let (transitions, transitions_from_any, transitions_from_shift) = transitions.into_iter().fold(
+      (HashMap::new(), vec![], HashMap::new()),
+      |(mut map, mut anys, mut shifts), transition| {
+        Self::split_transition(transition, &mut map, &mut anys, &mut shifts);
+        (map, anys, shifts)
+      },
+    );
+    Self {
+      current_state: State::enter_state(),
+      transitions,
+      transitions_from_any,
+      transitions_from_shift,
+    }
+  }
+  fn split_transition(
+    transition: Transition<State, Inputs, Shift>,
+    transitions: &mut HashMap<State, Vec<Transition<State, Inputs, Shift>>>,
+    any_transitions: &mut Vec<Transition<State, Inputs, Shift>>,
+    shift_transitions: &mut HashMap<Shift, Transition<State, Inputs, Shift>>,
+  ) {
+    match &transition {
+      Transition {
+        shift: Some(shift), ..
+      } => {
+        shift_transitions.insert(*shift, transition);
+      }
+      Transition { to, .. } if to == &State::any_state() => {
+        any_transitions.push(transition);
+      }
+      _ => transitions
+        .entry(transition.from.clone())
+        .or_default()
+        .push(transition),
+    };
+  }
+  pub fn current_state(&self) -> &State {
+    &self.current_state
+  }
+  fn get_transitions_for(
+    &mut self,
+    state: &State,
+    is_current_animation_finished: bool,
+    shift: &Option<Shift>,
+  ) -> Option<Vec<Transition<State, Inputs, Shift>>> {
+    if_chain! {
+      if let Some(shift) = shift;
+      if let Some(transition) = self.transitions_from_shift.get(shift);
+      then {
+        return Some(vec![transition.clone()]);
+      }
+    }
+    let transitions_for_any = self
+      .transitions_from_any
+      .iter()
+      .filter(|t| t.to != *state)
+      .cloned()
+      .collect::<Vec<_>>();
+    if let Some(transitions_for_state) = self.transitions.get_mut(state) {
+      transitions_for_state.sort_by_key(|transition: &Transition<State, Inputs, Shift>| {
+        match transition.condition.is_some() {
+          true => 1,
+          false => 0,
+        }
+      });
+      /*
+         | wait_until_finished | is_current_animation_finished | result |
+         |---------------------|-------------------------------|--------|
+         | true                | true                          | true   |
+         | true                | false                         | false  |
+         | false               | true                          | true   |
+         | false               | false                         | true   |
+      */
+      transitions_for_state.extend(transitions_for_any);
+      let transitions = transitions_for_state
+        .iter()
+        .filter(|t| !t.wait_until_finished || is_current_animation_finished)
+        .cloned()
+        .collect();
+      Some(transitions)
+    } else if !transitions_for_any.is_empty() {
+      Some(transitions_for_any)
+    } else {
+      None
+    }
+  }
+
+  pub fn step(
+    &mut self,
+    inputs: &Inputs,
+    is_current_animation_finished: bool,
+    next_shift: Option<Shift>,
+  ) -> AnimationStepResult<State> {
+    debug!("TODO: HANDLE {next_shift:?}");
+    let current_state = self.current_state.clone();
+    let mut changed = false;
+
+    let Some(valid_transitions) =
+      self.get_transitions_for(&current_state, is_current_animation_finished, &next_shift)
+    else {
+      return AnimationStepResult {
+        state: current_state,
+        changed,
+      };
+    };
+
+    let mut transition = None;
+    for t in valid_transitions {
+      match &t.condition {
+        None => {
+          transition = Some(t);
+          break;
+        }
+        Some(c) => {
+          if c.evaluate(inputs) {
+            transition = Some(t);
+            break;
+          }
+        }
+      }
+    }
+
+    if let Some(t) = transition {
+      self.current_state = t.to.clone();
+      changed = true;
+    }
+
+    if self.current_state == State::exit_state() {
+      self.current_state = State::enter_state();
+      let mut result = self.step(inputs, is_current_animation_finished, next_shift);
+      result.changed = true;
+      result
+    } else {
+      AnimationStepResult {
+        state: current_state,
+        changed,
+      }
+    }
+  }
+}
+#[macro_export]
+macro_rules! transition {
+  // no condition, no shift
+  ($from:ident -> $to:ident) => {
+    Transition::new(State::$from, State::$to, false, None, None)
+  };
+  ($from:ident |-> $to:ident) => {
+    Transition::new(State::$from, State::$to, true, None, None)
+  };
+
+  // yes condition, no shift
+  ($from:ident -> $to:ident when $condition:expr) => {
+    Transition::new(State::$from, State::$to, false, Some($condition), None)
+  };
+  ($from:ident |-> $to:ident when $condition:expr) => {
+    Transition::new(State::$from, State::$to, true, Some($condition), None)
+  };
+
+  // no condition, yes shift
+  (-> $to:ident on $shift:expr) => {
+    Transition::new(State::any_state(), State::$to, false, None, Some($shift))
+  };
+}
+use if_chain::if_chain;
+pub use transition;
---
+++ crates/macros/src/lib.rs
@@ -0,0 +1,335 @@
+use core::panic;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parse, parse_macro_input, punctuated::Punctuated, DeriveInput, Ident, Token};
+
+#[proc_macro_derive(AnimationEvent)]
+pub fn animation_event_derive(event: TokenStream) -> TokenStream {
+  animation_event_derive_impl(event)
+}
+
+fn animation_event_derive_impl(event: TokenStream) -> TokenStream {
+  let DeriveInput { ident, .. } = parse_macro_input!(event as DeriveInput);
+  quote! {
+      impl AnimationEvent for #ident {}
+  }
+  .into()
+}
+
+#[proc_macro_derive(AnimationShift)]
+pub fn animation_shift_derive(event: TokenStream) -> TokenStream {
+  animation_shift_derive_impl(event)
+}
+
+fn animation_shift_derive_impl(event: TokenStream) -> TokenStream {
+  let DeriveInput { ident, .. } = parse_macro_input!(event as DeriveInput);
+  quote! {
+      impl AnimationShift for #ident {}
+  }
+  .into()
+}
+#[proc_macro_derive(AnimationInput)]
+pub fn animation_input_derive(input: TokenStream) -> TokenStream {
+  animation_input_derive_impl(input)
+}
+fn animation_input_derive_impl(input: TokenStream) -> TokenStream {
+  let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
+  use syn::Data::*;
+  let (vars_enum_ident, vars_enum, get_impl) = match data {
+    Enum(_) | Union(_) => panic!("Enums and unions are not supported"),
+
+    Struct(syn::DataStruct {
+      fields: syn::Fields::Unnamed(_) | syn::Fields::Unit,
+      ..
+    }) => panic!("Structs with unnamed fields are not supported"),
+    Struct(syn::DataStruct {
+      fields: syn::Fields::Named(fields),
+      ..
+    }) => generate_vars_struct(fields),
+  };
+  quote! {
+      #vars_enum
+      impl AnimationInput for #ident {
+          type Vars = #vars_enum_ident;
+
+          fn get(&self, var: &Self::Vars) -> InputValue {
+              match *var {
+                  #(#get_impl),*
+              }
+          }
+
+      }
+  }
+  .into()
+}
+
+fn generate_vars_struct(
+  fields: syn::FieldsNamed,
+) -> (
+  syn::Ident,
+  proc_macro2::TokenStream,
+  Vec<proc_macro2::TokenStream>,
+) {
+  let vars: Vec<syn::Ident> = fields
+    .named
+    .iter()
+    .map(|field| {
+      let ident = field.ident.as_ref().expect("ident is none").to_string();
+      let name: Vec<&str> = ident.split("_").collect();
+      let (verb, adjective) = if name.len() == 2 {
+        (name[0].to_string(), name[1].to_string())
+      } else {
+        panic!("field name is not valid")
+      };
+      let verb = capitalize(&verb);
+      let adjective = capitalize(&adjective);
+      let var_name = format!("{}{}", verb, adjective);
+      syn::Ident::new(&var_name, proc_macro2::Span::call_site())
+    })
+    .collect();
+  let ident = syn::Ident::new("Vars", proc_macro2::Span::call_site());
+  let mut get_impls: Vec<proc_macro2::TokenStream> = fields
+    .named
+    .iter()
+    .zip(vars.iter())
+    .map(|(field, var)| {
+      let input_value = choose_inputvalue(field);
+      let field = field.ident.as_ref().unwrap();
+      quote! {
+          #ident::#var => {
+              InputValue::#input_value(self.#field)
+          }
+      }
+    })
+    .collect();
+  get_impls.push(quote! {
+      _ => {
+          unreachable!()
+      }
+  });
+  let enum_struct = quote! {
+      #[derive(Clone, Debug)]
+      pub enum #ident {
+          #(#vars,)*
+      }
+  };
+
+  (ident, enum_struct, get_impls)
+}
+fn choose_inputvalue(field: &syn::Field) -> syn::Ident {
+  match &field.ty {
+    syn::Type::Path(syn::TypePath { path, .. }) => {
+      let ty_ident = &path.segments.first().unwrap().ident;
+      match ty_ident.to_string().as_str() {
+        "bool" => syn::Ident::new("Boolean", proc_macro2::Span::call_site()),
+        "f32" => syn::Ident::new("Float", proc_macro2::Span::call_site()),
+        _ => panic!("field type is not bool or float"),
+      }
+    }
+    _ => panic!("field type is not bool"),
+  }
+}
+fn capitalize(s: &str) -> String {
+  let mut c = s.chars();
+  match c.next() {
+    None => String::new(),
+    Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+  }
+}
+
+#[proc_macro_attribute]
+pub fn animation_state(_content: TokenStream, input: TokenStream) -> TokenStream {
+  animation_state_impl(input)
+}
+fn animation_state_impl(input: TokenStream) -> TokenStream {
+  let item = parse_macro_input!(input as syn::Item);
+  let (e, t) = match item {
+    syn::Item::Enum(mut item) => {
+      //variants to add: Enter, Exit, Any
+      let variants_to_add: Vec<syn::Variant> = vec![quote! {Enter}, quote! {Exit}, quote! {Any}]
+        .into_iter()
+        .map(|v| syn::parse2(v).unwrap())
+        .collect();
+
+      let ident = item.ident.clone();
+      item.variants.extend(variants_to_add);
+      (
+        syn::Item::Enum(item.clone()),
+        quote! {
+            impl AnimationState for #ident {
+                fn enter_state() -> Self {
+                    Self::Enter
+                }
+                fn exit_state() -> Self {
+                    Self::Exit
+                }
+                fn any_state() -> Self {
+                    Self::Any
+                }
+            }
+        },
+      )
+    }
+    _ => panic!("Only enums are supported"),
+  };
+  quote! {
+      #e
+      #t
+  }
+  .into()
+}
+#[derive(Clone)]
+enum Arrow {
+  #[allow(dead_code)]
+  Then(Token![->]),
+  #[allow(dead_code)]
+  ThenWait((Token![|], Token![->])),
+}
+impl Parse for Arrow {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let lookahead = input.lookahead1();
+    if lookahead.peek(Token![|]) {
+      Ok(Self::ThenWait((input.parse()?, input.parse()?)))
+    } else {
+      Ok(Self::Then(input.parse()?))
+    }
+  }
+}
+#[derive(Clone)]
+
+enum Condition {
+  IsTrue(Ident),
+  IsFalse((Token![!], Ident)),
+}
+impl Parse for Condition {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let lookahead = input.lookahead1();
+    if lookahead.peek(Token![!]) {
+      Ok(Self::IsFalse((input.parse()?, input.parse()?)))
+    } else {
+      Ok(Self::IsTrue(input.parse()?))
+    }
+  }
+}
+#[derive(Clone)]
+
+struct Shift {
+  shift: Ident,
+}
+impl Parse for Shift {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    Ok(Shift {
+      shift: input.parse()?,
+    })
+  }
+}
+#[derive(Clone)]
+enum After {
+  Condition(Condition),
+  Shift(Shift),
+}
+impl Parse for After {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let ident = input.call(Ident::parse)?;
+    if ident == "when" {
+      Ok(After::Condition(input.parse()?))
+    } else if ident == "on" {
+      let shift = Shift {
+        shift: input.parse()?,
+      };
+      Ok(After::Shift(shift))
+    } else {
+      Err(syn::Error::new_spanned(
+        ident,
+        "should be either 'when' or 'on'",
+      ))
+    }
+  }
+}
+
+struct Transition {
+  start_state: Ident,
+  arrow: Arrow,
+  end_state: Ident,
+  after: Option<After>,
+}
+impl Parse for Transition {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    let mut transition = Transition {
+      start_state: input.parse()?,
+      arrow: input.parse()?,
+      end_state: input.parse()?,
+      after: None,
+    };
+    let lookahead = input.lookahead1();
+    if lookahead.peek(Ident) {
+      let after: After = input.parse()?;
+      transition.after = Some(after)
+    }
+    Ok(transition)
+  }
+}
+struct Transitions {
+  transitions: Punctuated<Transition, Token![,]>,
+}
+impl Parse for Transitions {
+  fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    Ok(Transitions {
+      transitions: input.parse_terminated(Transition::parse, Token![,])?,
+    })
+  }
+}
+#[proc_macro]
+pub fn transitions(item: TokenStream) -> TokenStream {
+  transitions_impl(item)
+}
+fn camelize(ident: Ident) -> Ident {
+  use stringcase::Caser;
+  let value = ident.to_string().to_pascal_case();
+  let camel_text = value.as_str();
+  Ident::new(camel_text, ident.span())
+}
+fn transitions_impl(item: TokenStream) -> TokenStream {
+  let input = parse_macro_input!(item as Transitions);
+  let transitions = input.transitions.iter().map(|transition| {
+    let from = transition.start_state.clone();
+    let to = transition.end_state.clone();
+    let wait = matches!(transition.arrow, Arrow::ThenWait(_));
+    let (condition, shift) = match transition.after.clone() {
+      None => (quote!(None), quote!(None)),
+      Some(After::Condition(cond)) => {
+        let cond = match cond {
+          Condition::IsTrue(ident) => {
+            let camel_ident = camelize(ident);
+            quote!(
+                Condition::IsTrue(Vars::#camel_ident)
+            )
+          }
+          Condition::IsFalse((_, ident)) => {
+            let camel_ident = camelize(ident);
+
+            quote!(
+                Condition::IsFalse(Vars::#camel_ident)
+            )
+          }
+        };
+        (quote! {Some(#cond)}, quote!(None))
+      }
+      Some(After::Shift(Shift { shift })) => {
+        let ident = camelize(shift);
+        let shift = quote! {
+            Shift::#ident
+        };
+        (quote!(None), quote!(Some(#shift)))
+      }
+    };
+    quote! {
+        Transition::new(State::#from, State::#to, #wait, #condition, #shift)
+    }
+  });
+  let transition = quote! {
+      vec![#(#transitions),*]
+  };
+  transition.into()
+}
