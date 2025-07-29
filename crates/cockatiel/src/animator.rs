dae5ec3b4997fa02da91df3e8eb8c1d4b014aa55
Kimberly <richardstuitx@gmail.com>
2025-07-29 19:56:57 +0200

    Add AnimationDirection enum

--- crates/cockatiel/src/animator.rs
+++ crates/cockatiel/src/animator.rs
@@ -253,39 +253,62 @@ impl<Tag: AnimatorTag> From<Animation<Tag::Event>> for AnimationGroup<Tag> {
     }
   }
 }
+
+enum AnimationDirection {
+  Paused,
+  Forward,
+  Backward,
+}
+impl From<f32> for AnimationDirection {
+  fn from(value: f32) -> Self {
+    if value == 0.0 {
+      AnimationDirection::Paused
+    } else if value > 0.0 {
+      AnimationDirection::Forward
+    } else {
+      AnimationDirection::Backward
+    }
+  }
+}
+impl Into<i32> for AnimationDirection {
+  fn into(self) -> i32 {
+    match self {
+      AnimationDirection::Paused => 0,
+      AnimationDirection::Forward => 1,
+      AnimationDirection::Backward => -1,
+    }
+  }
+}
+
 #[derive(Debug, Reflect, Clone, Default, PartialEq)]
 pub struct AnimationTimer {
   current: f32,
   duration: f32,
 }
-
 impl AnimationTimer {
-  fn new(duration: f32) -> Self {
-    AnimationTimer {
-      current: 0.,
-      duration,
-    }
+  fn new(duration: f32, direction: AnimationDirection) -> Self {
+    Self::with_offset(duration, direction, 0.0)
   }
-  fn new_at(start: f32, duration: f32) -> Self {
-    AnimationTimer {
-      current: start,
-      duration,
-    }
+
+  fn with_offset(duration: f32, direction: AnimationDirection, offset: f32) -> Self {
+    let current = match direction {
+      AnimationDirection::Paused => offset,
+      AnimationDirection::Forward => offset,
+      AnimationDirection::Backward => duration - offset,
+    };
+
+    AnimationTimer { current, duration }
   }
 
   fn tick(&mut self, time: f32) {
     self.current += time;
   }
 
-  fn finished(&self) -> bool {
-    self.current > self.duration || self.current < 0.
-  }
-
-  fn remainder(&self, forward: bool) -> f32 {
-    if forward {
-      self.current - self.duration
-    } else {
-      self.current
+  fn finished(&self, direction: AnimationDirection) -> bool {
+    match direction {
+      AnimationDirection::Paused => false,
+      AnimationDirection::Forward => self.current >= self.duration,
+      AnimationDirection::Backward => self.current <= 0.0,
     }
   }
 }
@@ -341,7 +364,7 @@ impl<Tag: AnimatorTag> Default for Animator<Tag> {
     }
     let transitions = Tag::transitions();
     Self {
-      timer: AnimationTimer::new(0.0),
+      timer: AnimationTimer::new(0.0, AnimationDirection::Forward),
       animations,
       state_machine: Machine::new(transitions, Tag::log()),
       frame_index: 0,
@@ -363,24 +386,33 @@ impl<Tag: AnimatorTag> Animator<Tag> {
 
   fn reset(&mut self, atlas: &mut TextureAtlas, frame_data: &FrameData<Tag::Event>, speed: f32) {
     self.frame_index = 0;
-    let remainder = self.timer.remainder(speed > 0.0);
-
-    self.start_timer(frame_data, remainder);
     atlas.index = frame_data
       .frames
       .first()
       .expect("no frame to reset to")
       .index;
+
+    let duration = self.get_frame_duration(frame_data);
+    let direction = speed.into();
+
+    self.timer = AnimationTimer::new(duration, direction);
   }
 
-  fn advance_frame_index(&mut self, frame_data: &FrameData<Tag::Event>, animation_direction: i32) {
+  fn advance_frame_index(
+    &mut self,
+    frame_data: &FrameData<Tag::Event>,
+    direction: AnimationDirection,
+  ) {
     let frame_amount = frame_data.frames.len() as i32;
     let old_frame_index = self.frame_index as i32;
+    let direction: i32 = direction.into();
+
     let frame_index = if frame_data.loops {
-      (old_frame_index + frame_amount + animation_direction) % frame_amount
+      (old_frame_index + frame_amount + direction) % frame_amount
     } else {
-      i32::clamp(old_frame_index + animation_direction, 0, frame_amount - 1)
+      i32::clamp(old_frame_index + direction, 0, frame_amount - 1)
     };
+
     if let Some(name) = Tag::log() {
       info!("[{name}] old_frame_index = {old_frame_index}, new_frame_index = {frame_index}")
     }
@@ -404,26 +436,25 @@ impl<Tag: AnimatorTag> Animator<Tag> {
   fn next<'a>(
     &mut self,
     frame_data: &'a FrameData<Tag::Event>,
-    animation_direction: i32,
+    direction: AnimationDirection,
   ) -> &'a Frame<Tag::Event> {
-    self.advance_frame_index(frame_data, animation_direction);
+    self.advance_frame_index(frame_data, direction);
     self.get_current_frame(frame_data)
   }
 
-  fn start_timer(&mut self, frame_data: &FrameData<Tag::Event>, start_at: f32) {
-    self.timer = AnimationTimer::new_at(
-      start_at,
-      frame_data
-        .frames
-        .get(self.frame_index)
-        .expect("frame_index was out of bounds when starting timer")
-        .duration
-        .as_secs_f32(),
-    );
+  fn get_frame_duration(&self, frame_data: &FrameData<Tag::Event>) -> f32 {
+    frame_data
+      .frames
+      .get(self.frame_index)
+      .expect("frame_index was out of bounds when starting timer")
+      .duration
+      .as_secs_f32()
   }
+
   fn get_animation_group(&self) -> Option<&AnimationGroup<Tag>> {
     self.animations.get(self.state_machine.current_state())
   }
+
   fn get_animation(&self) -> Option<&Animation<Tag::Event>> {
     //SAFE: animation_index can only be changed via `change_animation`, and that checks that it is inbounds
     let animation_group = self.animations.get(self.state_machine.current_state())?;
@@ -454,13 +485,14 @@ pub fn execute_animations<Tag: AnimatorTag>(
   )>,
   mut event_writer: EventWriter<AnimationEvent<Tag::Event>>,
 ) {
-  for (entity, mut animator, mut sprite, direction) in &mut query {
+  for (entity, mut animator, mut sprite, look_dir) in &mut query {
     if let Some(atlas) = &mut sprite.texture_atlas {
       let inputs = animator.inputs.clone();
       // if let Some(tag_name) = Tag::log() {
       //   bevy::log::info!("[{tag_name}] Animation state is: {old_state:?}");
       // }
       // Required for animations which need to play fully before transitioning
+
       //TODO: dont unwrap
       let speed = if let Some(animation_group) = animator.get_animation_group() {
         animation_group.speed(&animator.inputs)
@@ -469,7 +501,7 @@ pub fn execute_animations<Tag: AnimatorTag>(
       };
 
       let is_last_frame = animator
-        .get_frames(direction)
+        .get_frames(look_dir)
         .map(|f| f.frames.len() - 1 == animator.frame_index)
         .unwrap_or(true);
 
@@ -480,33 +512,34 @@ pub fn execute_animations<Tag: AnimatorTag>(
           time.delta_secs() * speed
         )
       }
-      // ticking earlier cause I need the
       animator.timer.tick(time.delta_secs() * speed);
 
       let shift = animator.next_shift.take();
-      let has_animation_finished = animator.timer.finished();
+      let has_animation_finished = animator.timer.finished(speed.into());
+
       let step_result =
         animator
           .state_machine
           .step(&inputs, is_last_frame && has_animation_finished, shift);
-      let frame_data = animator.get_frames(direction).clone();
+
+      let frame_data = animator.get_frames(look_dir).clone();
       if let Some(frame_data) = frame_data {
         let current_state = animator.state_machine.current_state();
         if step_result.changed {
           if let Some(tag_name) = Tag::log() {
             bevy::log::info!("[{tag_name}] Animation state changed to {current_state:?} with inputs: {inputs:?} (step_result: {step_result:?})");
-            bevy::log::info!("[{tag_name}] look_direction: {direction:?}");
+            bevy::log::info!("[{tag_name}] look_direction: {look_dir:?}");
           }
 
           animator.reset(atlas, &frame_data, speed);
         }
 
         if has_animation_finished {
-          let frame = animator.next(&frame_data, speed.signum_i32());
+          let frame = animator.next(&frame_data, speed.into());
           atlas.index = frame.index;
 
           if let Some(animation) = animator.get_animation() {
-            sprite.flip_x = animation.flip_x(direction);
+            sprite.flip_x = animation.flip_x(look_dir);
           }
 
           if let Some(ref event) = frame.event {
@@ -516,12 +549,15 @@ pub fn execute_animations<Tag: AnimatorTag>(
             };
             event_writer.send(animator_event);
           }
-          let remainder = animator.timer.remainder(speed > 0.0);
-          animator.start_timer(&frame_data, remainder);
+
+          let duration = animator.get_frame_duration(&frame_data);
+          let offset = animator.timer.current;
+          animator.timer = AnimationTimer::with_offset(duration, speed.into(), offset);
+
           if let Some(name) = Tag::log() {
             info!(
               "[{name}] remainder: {}, new timer: {:?}",
-              remainder, animator.timer,
+              offset, animator.timer,
             )
           }
         }
@@ -552,17 +588,17 @@ pub fn sync_animations<Tag: AnimatorTag>(
   mut targets: Query<(&mut Sprite, &mut DerivedAnimator<Tag>)>,
 ) {
   for (mut sprite, target) in &mut targets {
-    let Ok((source, direction)) = sources.get(target.animator_target) else {
+    let Ok((source, look_dir)) = sources.get(target.animator_target) else {
       continue;
     };
 
     let current_state = source.state_machine.current_state();
     if_chain! {
       if let Some(animation) = target.animations.get(current_state);
-      if let Some(frame_data) = animation.get(direction);
+      if let Some(frame_data) = animation.get(look_dir);
       if let Some(frame) = frame_data.frames.get(source.frame_index);
       then {
-        sprite.flip_x = animation.flip_x(direction);
+        sprite.flip_x = animation.flip_x(look_dir);
         if let Some(atlas) = &mut sprite.texture_atlas {
             atlas.index = frame.index;
         }
